785958f542029a3113b338bdf636ab5f5131725c
Sun Lei <larsson@SundeMacBook.local>
2020-03-26 15:45:28 +0800

    Added the demo. Get the microservice from api-gateway (Consul) and call it.

--- MyFirstgRPCProj.Client.Con/Program.cs
+++ MyFirstgRPCProj.Client.Con/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Consul;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Net.Client;
 using MyFirstgRPCProj.Server;
@@ -16,35 +17,63 @@ namespace MyFirstgRPCProj.Client.Con
             AppContext.SetSwitch(
                 "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
 
-            // The port number(5000) must match the port of the gRPC server.
-            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            #region  non-api-gateway
+
+            // // The port number(5000) must match the port of the gRPC server.
+            // var channel = GrpcChannel.ForAddress("http://localhost:5000");
 
             // var client = new Greeter.GreeterClient(channel);
             // var reply = await client.SayHelloAsync(new HelloRequest { Name = "Larsson" });
             // await channel.ShutdownAsync();
             // Console.WriteLine($"服务Greeter返回的数据：{reply.Message}");
 
-            // var client = new FukinBullShiter.FukinBullShiterClient(channel);
-            // var reply = await client.FukUAsync(new FukWhoRequest { Name = "Larsson", Age = 39 });
+            // var client2 = new FukinBullShiter.FukinBullShiterClient(channel);
+            // var reply2 = await client2.FukUAsync(new FukWhoRequest { Name = "Larsson", Age = 39 });
             // await channel.ShutdownAsync();
-            // Console.WriteLine($"{reply.Mesage}");
+            // Console.WriteLine($"{reply2.Mesage}");
 
-            var client = new ScetiaItemSourceGetter.ScetiaItemSourceGetterClient(channel);
-            // var reply = await client.GetKindsAsync(new Empty());
+            // var client3 = new ScetiaItemSourceGetter.ScetiaItemSourceGetterClient(channel);
+            // var reply3 = await client3.GetKindsAsync(new Empty());
             // await channel.ShutdownAsync();
-            // reply.Kinds.Any(x =>
+            // reply3.Kinds.Any(x =>
             // {
             //     Console.WriteLine($"{x.KindId}-{x.KindName}{(x.CanConsign ? string.Empty : "(不可收样)")}");
             //     return false;
             // });
 
+            #endregion
 
-            var request = new KindsRequest();
-            request.Kinds.Add(new Kind { KindId = "21", KindName = "垃圾", CanConsign = false });
-            var reply = await client.SetKindsAsync(request);
-            await channel.ShutdownAsync();
-            Console.WriteLine($"result：{reply.Result}. kindTotalCount：{reply.TotalCount}");
+            #region api-gateway inuse
 
+            var serviceAddress = string.Empty;
+
+            using (var consulClient = new ConsulClient(option => option.Address = new Uri("http://localhost:8500")))
+            {
+                var serviceName = "MyFirstgRPCMicroServ";
+                var services = await consulClient.Catalog.Service(serviceName);
+                if (services.Response.Length == 0)
+                {
+                    Console.WriteLine($"can not find the service \"{serviceName}\"");
+                    return;
+                }
+
+                var service = services.Response[0];
+                serviceAddress = $"http://{service.ServiceAddress}:{service.ServicePort}";
+                Console.WriteLine($"service \"{serviceName}\" load succeed.");
+            }
+
+            BoolReply reply5 = null;
+            using (var channelFromConsul = GrpcChannel.ForAddress(serviceAddress))
+            {
+                var serviceClient = new ScetiaItemSourceGetter.ScetiaItemSourceGetterClient(channelFromConsul);
+                var request = new KindsRequest();
+                request.Kinds.Add(new Kind { KindId = "21", KindName = "垃圾", CanConsign = false });
+                reply5 = await serviceClient.SetKindsAsync(request);
+                await channelFromConsul.ShutdownAsync();
+            }
+            Console.WriteLine($"result：{reply5.Result}. kindTotalCount：{reply5.TotalCount}");
+
+            #endregion
         }
     }
 }
